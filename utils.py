5605afc7d9d64dcfeeacf2c296655d0922df7cc2
rashmi <wwwrashmishinde2002@gmail.com>
2024-07-15 21:38:57 +0530

    optimizing space

--- utils.py
+++ utils.py
@@ -4,5 +4,3 @@ from config import Config
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in Config.ALLOWED_EXTENSIONS
-           
-#This is the configuration file"
